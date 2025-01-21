41e6d15236c8fad342e8865d156c13f3de828be5
Bayram Yelec <bayrammyelec@gmail.com>
2025-01-21 14:05:37 +0300

    commit

--- CustomHorizontalCalendar/Scene/Home/HomeVC.swift
+++ CustomHorizontalCalendar/Scene/Home/HomeVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  HomeVC.swift
 //  CustomHorizontalCalendar
 //
 //  Created by Bayram Yeleç on 21.01.2025.
@@ -7,13 +7,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class HomeVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBackground
     }
-
-
 }
-
